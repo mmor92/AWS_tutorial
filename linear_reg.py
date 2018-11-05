d2d6af6bff59e4fb4abd6fce93a19e92d55c571c
Marc Moreno <marcmoreno.92@gmail.com>
2018-11-05 14:13:32 -0700

    Updating linear regression

--- linear_reg.py
+++ linear_reg.py
@@ -27,19 +27,16 @@ regr.fit(diabetes_X_train, diabetes_y_train)
 # Make predictions using the testing set
 diabetes_y_pred = regr.predict(diabetes_X_test)
 
+
+file = open("results.txt","w")
+
 # The coefficients
 print('Coefficients: \n', regr.coef_)
+file.write(regr.coef_)
 # The mean squared error
 print("Mean squared error: %.2f"
       % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+file.write(mean_squared_error(diabetes_y_test, diabetes_y_pred))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
-
-# Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
-
-plt.xticks(())
-plt.yticks(())
-
-plt.show()
+file.write(r2_score(diabetes_y_test, diabetes_y_pred))
